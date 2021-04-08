bcd93e5adb7cd4a10c51858372910f6fbd162bc9
Manikandan <mani89in@gmail.com>
2021-04-08 18:17:33 +0530

    DS - Array Rotation problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArraysService } from './arrays/arrays.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ArraysService],
 })
 export class AppModule {}
